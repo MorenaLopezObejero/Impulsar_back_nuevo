46614865bc5c469295dbee0fec6d44ff30ce23fb
MorenaLopezObejero <48111548@est.ort.edu.ar>
2025-07-10 17:10:54 -0300

    Algo de run build. Cambiarlo prox clase proyecto

---
+++ dist/index.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+// Configuración express
+const app = (0, express_1.default)();
+const PORT = 8000;
+// Middlewares
+const corsOptions = {
+    origin: '*', // permitir acceso desde cualquier origen
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // permitir métodos HTTP
+    allowedHeaders: ['Content-Type', 'Authorization'], // permitir headers
+};
+app.use((0, cors_1.default)(corsOptions));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.listen(PORT, () => {
+    console.log(`✅ Server is running on port ${PORT}`);
+});
+app.get("/", (req, res) => {
+    res.json({ message: "Funca" });
+});
