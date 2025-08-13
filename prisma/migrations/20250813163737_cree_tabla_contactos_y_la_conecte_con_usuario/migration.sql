2ade331c322c43a9ec7472551522987cf7913796
MorenaLopezObejero <48111548@est.ort.edu.ar>
2025-08-13 14:20:34 -0300

    conecte la tabla contactos con usuario y creee contacto.service

---
+++ prisma/migrations/20250813163737_cree_tabla_contactos_y_la_conecte_con_usuario/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "Contactos" (
+    "Id" SERIAL NOT NULL,
+    "propietario" TEXT NOT NULL,
+    "nombre" TEXT,
+    "email" TEXT,
+    "telefono" INTEGER,
+    "redes_soc" TEXT,
+    "notas_perz" TEXT,
+    "foto" TEXT,
+    "host" TEXT NOT NULL,
+
+    CONSTRAINT "Contactos_pkey" PRIMARY KEY ("Id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Contactos" ADD CONSTRAINT "Contactos_host_fkey" FOREIGN KEY ("host") REFERENCES "Usuario"("email") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ prisma/migrations/20250813164544_cambie_algo_minimo_en_la_tabla_contactos/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `propietario` on the `Contactos` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Contactos" DROP COLUMN "propietario";
