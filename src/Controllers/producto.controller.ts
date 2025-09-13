9ee42a1a0de4dcd023e5e3cc0c7bfb2d6ce5a180
Tomaslevys <141672471+Tomaslevys@users.noreply.github.com>
2025-09-13 13:30:07 -0300

    CAMBIOS EN EL  BACK

--- src/Controllers/producto.controller.ts
+++ src/Controllers/producto.controller.ts
@@ -4,12 +4,14 @@ import { Request, Response } from 'express';
 
 export const getProductobyTipoController = async (req:Request, res:Response) => {
     try {
-        const producto = await getProductobyTipoService(req.params.Typo_emprendimientos);
+        const producto = await getProductobyTipoService(req.params.Typo_emprendimiento);
         res.json(producto);
-    } catch (err) {
-        res.status(500).json({error: 'Error al obtener productos'});
-        throw err;
-    }
+    } // --- CÓDIGO CORREGIDO ---
+catch (err) {
+    console.error("--- ¡ERROR REAL DE PRISMA! ---", err); // Agregamos esto para ver el error en la terminal
+    res.status(500).json({error: 'Error al obtener productos'});
+    // Quitamos el 'throw err;' para que el servidor no se cuelgue
+}
 };
 
 export const getMaterialbyProductoController = async (req: Request, res: Response) => {
