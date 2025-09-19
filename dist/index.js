fca212c3ef779798f42a747d6f0ac42881f90876
Tomaslevys <163904002+MorenaLopezObejero@users.noreply.github.com>
2025-09-19 19:35:48 -0300

    PRUEBA

--- dist/index.js
+++ dist/index.js
@@ -31,3 +31,5 @@ app.use('/Materiales_CyS', materiales_cys_routes_1.default);
 const productos_cys_routes_1 = __importDefault(require("./Routes/productos_cys.routes"));
 app.use('/Productos_CyS', productos_cys_routes_1.default);
 exports.default = app;
+
+//X
