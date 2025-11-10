311e5f9cd912fd11ad03aee20f93ce7612b1d9f2
MorenaLopezObejero <48111548@est.ort.edu.ar>
2025-11-10 14:25:19 -0300

    agregue jswebtoken a login

--- src/Controllers/usuario.controllers.ts
+++ src/Controllers/usuario.controllers.ts
@@ -1,5 +1,6 @@
 import { error } from "console";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import { postUsuarioService, getUsuarioService} from "../Services/usuario.service";
 import { Request, Response } from 'express';
 
@@ -7,6 +8,7 @@ export const getUsuarioController = async (req: Request, res: Response) => {
     try{
         const usuario = await getUsuarioService(req.body.email);
         res.json(usuario);
+        
     } catch (err) {
         res.status(500).json({error: 'Error al crear Usuario'});
         throw err;
@@ -15,8 +17,9 @@ export const getUsuarioController = async (req: Request, res: Response) => {
 
 export const postUsuarioController1 = async (req:Request, res:Response) => {
     try {
-        const usuario = await postUsuarioService(req.body.email, req.body.contasena, req.body.nombreUsu);
-        res.json(usuario);
+        const usuario = await postUsuarioService(req.body.email, req.body.contrasena, req.body.nombreUsu);
+        const token = jwt.sign({ email: usuario.email}, process.env.JWT_SECRET as string);
+        return res.status(201).json({ message: 'Usuario registrado con éxito.', token, user: { email: usuario.email} });
     } catch (err) {
         res.status(500).json({error: 'Error al crear Usuario'});
         throw err;
--- src/Routes/usuario.routes.ts
+++ src/Routes/usuario.routes.ts
@@ -1,5 +1,6 @@
 import  express  from "express";
 import { postUsuarioController1, getUsuarioController} from "../Controllers/usuario.controllers";
+import authMidd from '../Middleware/auth';
 
 const router = express.Router();
 
